c84f9bcc0d512c7f5fa37fb9d5b3e4b1f3c86377
JohnOkari <johnnokari@gmail.com>
2023-08-13 22:33:06 +0300

    add about content

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -22,7 +22,18 @@ const AboutUs = () => {
         </div>
 
         <div className="flex flex-col md:w-[50%] mt-12 md:mt-0 md:text-start text-center justify-center items-center gap-4">
-        
+          <p className="text-white p-2 ">
+            Experience the ultimate in car detailing excellence at Color &
+            Drive. With a genuine passion for cars, our dedicated team of
+            professionals goes above and beyond to deliver exceptional services.
+            Using cutting-edge techniques and premium products, we restore your
+            vehicle to its pristine condition. Our customizable packages cater
+            to your unique requirements, ensuring a tailored experience. From
+            initial contact to the moment you drive away, our commitment to
+            outstanding customer service is unwavering. Discover the
+            transformative power of our exceptional car detailing services by
+            scheduling an appointment with Color & Drive today.
+          </p>
           <div className="flex justify-center items-center gap-4">
             <button className="p-2 bg-[#FB0000] rounded-md text-white">
               Call To Book Now
